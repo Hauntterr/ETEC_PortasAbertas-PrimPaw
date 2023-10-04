efdb52d51c34b17979f850c4c48f416cf99aaf3c
Hauntterr <135064464+Hauntterr@users.noreply.github.com>
2023-10-04 07:53:44 -0300

    Add files via upload

---
+++ primpawAPI/primpawAPI/Produto.cs
@@ -0,0 +1,10 @@
+﻿namespace primpawAPI
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Img { get; set; }
+        public decimal Preco { get; set; }
+    }
+}
