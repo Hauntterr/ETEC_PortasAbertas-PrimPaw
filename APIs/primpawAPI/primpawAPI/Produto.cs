41d99d51e9d3eb649121da958197c5027ed3587a
Hauntterr <135064464+Hauntterr@users.noreply.github.com>
2023-10-25 09:36:28 -0300

    Add files via upload

---
+++ APIs/primpawAPI/primpawAPI/Produto.cs
@@ -0,0 +1,16 @@
+﻿using System.Data.SqlClient;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace primpawAPI
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string Img { get; set; }
+        public string Nome { get; set; }
+        public decimal Preco { get; set; }
+    }
+
+
+    
+}
